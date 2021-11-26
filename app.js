5940af4000fda713e8063e45b7756c4c903656a4
DARSHIL SHAH <my.name@example.com>
2021-11-26 09:57:04 +0200

    Databse COnnection Successfully

--- app.js
+++ app.js
@@ -1,61 +1,30 @@
-const express = require("express");
+const express = require('express');
+const morgan = require('morgan');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
 const app = express();
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
 
-const productRoutes = require("./api/routes/products");
-const orderRoutes = require("./api/routes/orders");
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
-
-mongoose.connect('mongodb+srv://DAMZ:'+ process.env.MONGO_ATLAS_PW +'@nokia-garage.zcvd7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-).then(() => {
-  console.log('Database Connected');
-})
-.catch(err=>{
-    console.log('Database Not Connected' + err);
-});
-
-
-  
-app.use(morgan("dev"));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
+  console.log('Hello from the middleware 👋');
   next();
 });
 
-// Routes which should handle requests
-app.use("/products", productRoutes);
-app.use("/orders", orderRoutes);
-
 app.use((req, res, next) => {
-  const error = new Error("Not found");
-  error.status = 404;
-  next(error);
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
-app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({
-    error: {
-      message: error.message
-    }
-  });
-});
+// 3) ROUTES
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,8 +1,24 @@
-const http = require('http');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = require('./app');
 
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful!'));
+
 const port = process.env.PORT || 3000;
-
-const server = http.createServer(app);
-
-server.listen(port);
+app.listen(port,  () => {
+  console.log(`App running on port ${port}...`);
+});
